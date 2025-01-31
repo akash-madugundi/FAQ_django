33cdacbb3f1cf07813022e551937bb5b72420ff7
Akash <akashmadugundi@gmail.com>
2025-01-31 23:06:50 +0530

    feat: Add multilingual FAQ model (logic moved from views to models)

--- faqApp/models.py
+++ faqApp/models.py
@@ -1,10 +1,21 @@
 from django.db import models
 from ckeditor.fields import RichTextField
+from googletrans import Translator
 
-class FAQ(models.Model):   # FAQ is table name
+class FAQ(models.Model):
     faq_id = models.AutoField
     question = models.TextField()
     answer = RichTextField()
 
     # def __str__(self):
     #     return self.question
+    def translate(self, lang='en'):
+        translator = Translator()
+        translated_question = translator.translate(self.question, dest=lang).text
+        translated_answer = translator.translate(self.answer, dest=lang).text
+
+        return {
+            'id': self.id,
+            'question': translated_question,
+            'answer': translated_answer
+        }
--- faqApp/views.py
+++ faqApp/views.py
@@ -3,8 +3,6 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import FAQ
 from .api.serializers import FAQSerializer
-from googletrans import Translator
-
 # Create your views here.
 
 def home(request):
@@ -21,21 +19,7 @@ def home(request):
 def get_faqs(request):
     lang = request.GET.get('lang', 'en').strip() 
     faqs = FAQ.objects.all()
-    serializer = FAQSerializer(faqs, many=True)
-
-    if not lang:
-        lang = 'en'
-        
-    translator = Translator()
-    translated_data = []
-
-    for faq in serializer.data:
-        translated_question = translator.translate(faq['question'], dest=lang).text
-        translated_answer = translator.translate(faq['answer'], dest=lang).text
-        translated_data.append({
-            'id': faq['id'],
-            'question': translated_question,
-            'answer': translated_answer
-        })
+    
+    translated_data = [faq.translate(lang) for faq in faqs]
 
     return Response(translated_data)
